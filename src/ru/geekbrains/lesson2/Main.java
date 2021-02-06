7fdb88b7e4171c98d03c3c1d5430d3bd59fa77c0
Looch <looch@bk.ru>
2021-02-06 15:09:09 +0300

    Lesson 2 homework is done

---
+++ src/ru/geekbrains/lesson2/Main.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.lesson2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[][] numbers = new String[4][4];
+        System.out.println(Calculator.stringToIntCalcSafe(numbers));
+    }
+
+}
